56042481570649593e8b4e55b4ee2f79a6769795
Jimbxxx <jimbxxx.content@gmail.com>
2026-05-25 09:09:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+import os
 import uvicorn
 
 if __name__ == "__main__":
+
+    port = int(os.getenv("PORT", 8000))
+
     uvicorn.run(
         "api.client:app",
         host="0.0.0.0",
-        port=8000
+        port=port,
+        reload=False
     )
